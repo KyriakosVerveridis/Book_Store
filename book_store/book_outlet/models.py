107623025d43829a0398a87fb69bac3d03a340c6
KyriakosVerveridis <ververidiskyriakos@gmail.com>
2025-11-02 21:01:58 +0200

    Updating Models & Migrations

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,8 +1,25 @@
 from django.db import models
+from django.core.validators import MinValueValidator,MaxValueValidator
 
 # Create your models here.
 
 class Book(models.Model):
-  title = models.CharField(max_length=50)
-  rating = models.IntegerField()
+  """
+    Model representing a Book.
+
+    Changes made in this version:
+    - Added 'author' field (optional, max length 100)
+    - Added 'is_bestselling' boolean field (default=False)
+    - Added validators for 'rating' to ensure values between 1 and 5
+    """ 
+  
+  title = models.CharField(max_length=50) # Title of the book
+  rating = models.IntegerField(
+    validators=[MinValueValidator(1),MaxValueValidator(5)]
+    ) # Rating between 1 and 5
+  author = models.CharField(null=True,max_length=100) # Author's name, optional
+  is_bestselling = models.BooleanField(default=False) # True if the book is a bestseller
+
+  def __str__(self):
+    return f"{self.title} ({self.rating})"
   
