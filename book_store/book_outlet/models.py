dde590ce0e627c790b6d214f7938f476db4fb41c
KyriakosVerveridis <ververidiskyriakos@gmail.com>
2025-11-04 20:02:47 +0200

    Adding a Slugfield & Overwriting Save

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator,MaxValueValidator
 from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -12,7 +13,7 @@ class Book(models.Model):
     - Added 'author' field (optional, max length 100)
     - Added 'is_bestselling' boolean field (default=False)
     - Added validators for 'rating' to ensure values between 1 and 5
-    """ 
+  """ 
   
   title = models.CharField(max_length=50) # Title of the book
   rating = models.IntegerField(
@@ -21,6 +22,11 @@ class Book(models.Model):
   author = models.CharField(null=True,max_length=100) # Author's name, optional
   is_bestselling = models.BooleanField(default=False) # True if the book is a bestseller
 
+  """
+   Stores a URL-friendly version of the title,
+   generated automatically in save(), e.g., "Harry Potter 1" → "harry-potter-1"
+  """
+  slug = models.SlugField(default="",null=False)
 
   """
     Returns the canonical URL for this Book instance using Django's reverse() function.
@@ -29,6 +35,15 @@ class Book(models.Model):
   def get_absolute_url(self):
       return reverse("book-detail", args=[self.id])
   
+
+  def save(self,*args,**kwargs):
+     
+     # Automatically generates a URL-friendly version of the title, e.g., "My Book" → "my-book"
+     self.slug = slugify(self.title)
+
+     # Call the original save() method to save the instance to the database
+     super().save(*args, **kwargs)
+    
   
   def __str__(self):
     return f"{self.title} ({self.rating})"
