430dbaa874656fa6583c4b8d093fc154f37bd03f
KyriakosVerveridis <ververidiskyriakos@gmail.com>
2025-11-04 17:49:41 +0200

    Model URLs

--- book_store/book_outlet/urls.py
+++ book_store/book_outlet/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index , name="index"),
-    path("<int:id>", views.book_detail , name="book_detail"),
+    path("<int:id>", views.book_detail , name="book-detail"),
 
 ]
