54c7b54ecde938c15985dd064f7ae855c5c84bdc
KyriakosVerveridis <ververidiskyriakos@gmail.com>
2025-11-04 15:54:37 +0200

    Rendering the Details Page

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404,render
+from django.http import Http404
 from .models import Book
 
 # Create your views here.
@@ -13,3 +14,22 @@ def index(request):
   books = Book.objects.all()
   context = {"books": books}
   return render(request,"book_outlet/index.html",context)
+
+
+def book_detail(request,id):
+  """
+    Fetches a single Book by its primary key (pk) and passes its
+    title, author, rating, and bestseller status to the template.
+    """
+  
+  # Fetch the Book with the given primary key (pk) or return 404 if not found
+  book = get_object_or_404(Book,pk=id)
+  context = {
+    "title":book.title,
+    "author":book.author,
+    "rating":book.rating,
+    "is_bestseller":book.is_bestselling
+  }
+  return render(request,"book_outlet/book_detail.html",context)
+
+
