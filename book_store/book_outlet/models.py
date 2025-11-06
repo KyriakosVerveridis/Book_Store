f83e1df29e519116dc82bd08638b1d373aacc479
KyriakosVerveridis <ververidiskyriakos@gmail.com>
2025-11-06 23:22:05 +0200

    Setting-up many-to-many

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -3,6 +3,12 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
 from django.utils.text import slugify
 
+
+class Country(models.Model):
+    name = models.CharField(max_length=80)
+    code = models.CharField(max_length=2)
+
+
 class Address(models.Model):
     street = models.CharField(max_length=100)
     postal_code = models.CharField(max_length=5)
@@ -17,13 +23,14 @@ class Address(models.Model):
           as it will appear in the Django admin"""
         verbose_name_plural = "Address Entries"
 
+
 class Author(models.Model):
     """
     Represents an author with first and last name.
     """
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True) # Creates a one-to-one relationship with Address
 
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
@@ -31,6 +38,7 @@ class Author(models.Model):
     def __str__(self):
         """String representation of the Author object."""
         return self.full_name()
+
 
 class Book(models.Model):
     """
@@ -47,17 +55,11 @@ class Book(models.Model):
         validators=[MinValueValidator(1), MaxValueValidator(5)]
     )  # Rating between 1 και 5
     author = models.ForeignKey(Author, on_delete=models.CASCADE,null=True,
-                               related_name="books") # Link to an Author record
-    is_bestselling = models.BooleanField(default=False)  # True if the book is a bestseller
-
-    # Stores a URL-friendly version of the title.
-    # Automatically generated in the admin via prepopulated_fields.
-    slug = models.SlugField(
-        default="",
-        null=False,
-        blank=True,
-        db_index=True,
-    )
+                               related_name="books") # Link to an Author record.
+    is_bestselling = models.BooleanField(default=False)  # True if the book is a bestseller.
+    slug = models.SlugField(default="", null=False, blank=True, db_index=True) # Stores a URL-friendly version of the title.
+                                                                               # Automatically generated in the admin via prepopulated_fields. 
+    published_countries = models.ManyToManyField(Country) # Links the object to multiple countries
 
     def get_absolute_url(self):
         """
